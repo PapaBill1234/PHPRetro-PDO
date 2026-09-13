923951c0c7f079454aa38a5f355e7fe32fc46c59
PapaBill1234 <59884090+PapaBill1234@users.noreply.github.com>
2026-09-13 11:43:38 +0200

    [phase4][batch3] remove unused legacy database dependency

--- papers.php
+++ papers.php
@@ -1,48 +1,8 @@
 <?php
-/*================================================================+\
-|| # PHPRetro - An extendable virtual hotel site and management
-|+==================================================================
-|| # Copyright (C) 2009 Yifan Lu. All rights reserved.
-|| # http://www.yifanlu.com
-|| # Parts Copyright (C) 2009 Meth0d. All rights reserved.
-|| # http://www.meth0d.org
-|| # All images, scripts, and layouts
-|| # Copyright (C) 2009 Sulake Ltd. All rights reserved.
-|+==================================================================
-|| # PHPRetro is provided "as is" and comes without
-|| # warrenty of any kind. PHPRetro is free software!
-|| # License: GNU Public License 3.0
-|| # http://opensource.org/licenses/gpl-license.php
-\+================================================================*/
-
-require_once('./includes/core.php');
-$data = new index_sql;
-$lang->addLocale("landing.papers");
-
-$page['name'] = $lang->loc['pagename.home'];
-$page['bodyid'] = "landing";
-require_once('./templates/login_header.php');
-
-if($_GET['page'] == "disclaimer"){
-	$title = $lang->loc['disclaimer'];
-	$content = $settings->find("paper_disclaimer");
-}else{
-	$title = $lang->loc['privacy.policy'];
-	$content = $settings->find("paper_privacy");
-}
-
+require_once('./includes/core.php'); $lang->addLocale('landing.papers');
+$requested = (string) ($_GET['page'] ?? 'privacy');
+if ($requested === 'disclaimer') { $title = $lang->loc['disclaimer']; $content = $settings->find('paper_disclaimer'); } else { $title = $lang->loc['privacy.policy']; $content = $settings->find('paper_privacy'); }
+$page['name'] = $title; $page['bodyid'] = 'landing'; require_once('./templates/login_header.php');
 ?>
-
-<div id="process-content">
-	        	<div id="terms" class="box-content">
-<div class="tos-header"><b><?php echo $title; ?></b></div>
-<div class="tos-item"><?php echo $content; ?></div>
-</div>
-
-
-
-<?php
-
-require('./templates/login_footer.php');
-
-?>
+<div id="process-content"><div id="terms" class="box-content"><div class="tos-header"><b><?php echo HoloText($title); ?></b></div><div class="tos-item"><?php echo $content; ?></div></div></div>
+<?php require_once('./templates/login_footer.php'); ?>
