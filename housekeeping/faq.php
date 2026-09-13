24bcf13408c32feb9fc1b0cd3905b70bd1c8f192
PapaBill1234 <59884090+PapaBill1234@users.noreply.github.com>
2026-09-13 12:33:20 +0200

    Phase 5b: load audit helper for privileged actions

--- housekeeping/faq.php
+++ housekeeping/faq.php
@@ -1,6 +1,7 @@
 <?php
 $page['dir'] = '\\housekeeping'; $page['housekeeping'] = true; $page['rank'] = 5;
 require_once('../includes/core.php'); require_once('./includes/hksession.php');
+require_once('../includes/AdminAudit.php');
 $database = new Database(); $action = $_GET['do'] ?? 'list'; $notice = '';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id = (int) ($_POST['id'] ?? 0);
